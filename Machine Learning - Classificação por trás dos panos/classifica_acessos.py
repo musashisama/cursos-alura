4aa40f9c8b42d33d95b7ebf8d6bda705b7d229c3
musashisama <48837659+musashisama@users.noreply.github.com>
2019-04-18 11:15:10 -0300

    Novas lições

---
+++ Machine Learning - Classificação por trás dos panos/classifica_acessos.py
@@ -0,0 +1,29 @@
+from lesson1_carrega_csv import carregar_acessos
+from sklearn.naive_bayes import MultinomialNB
+
+X,Y = carregar_acessos()
+
+treino_dados = X[:90]
+treino_marcacoes = Y[:90]
+
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
+
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados,treino_marcacoes)
+
+print(modelo.predict([[1,0,1]]))
+
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+acertos = [d for d in diferencas if d == 0]
+print(acertos)
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0* total_de_acertos/total_de_elementos
+print(taxa_de_acerto)
+print(total_de_acertos)
+print(total_de_elementos)
